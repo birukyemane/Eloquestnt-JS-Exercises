cd52089d4432fc42e6ed88ccc03f638a60d3cce5
birukyemane <bir_yem@yahoo.com>
2018-09-11 15:32:56 +0300

    ch7 exercise 1 and 2

---
+++ chp7/main.js
@@ -0,0 +1,191 @@
+// files for the chapter taken from the book 
+// here are all the examples from the book 
+
+var roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+var roadGraph = buildGraph(roads);
+
+var VillageState = class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return {direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+var mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+
+/* Just to test. to see the state of the parcels 
+function display(parcels){
+  for (let parcel of parcels){
+    console.log(parcel.place + ' - ' + parcel.address);
+  }
+} */
+
+// exercise 2 
+
+function yourRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    route = findNearestParcel(roadGraph, place, parcels);
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+function findNearestParcel(graph, from, parcels) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (parcels.some(p => (p.place == place && p.place !== from) || (p.place == from && p.address == place))) {
+        return route.concat(place);
+      }
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+
+// exercise 1 
+
+// a version of runRobot to compare two robots. it returns the 'turn' 
+function runRobotCompare(state, robot, memory) { 
+  for (let turn = 0;; turn++) {
+    if (state.parcels.length == 0) {
+     // console.log(`Done in ${turn} turns`);
+     return turn; 
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    // console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  let totalTimeRobot1 = 0, totalTimeRobot2 = 0;
+  for (let i = 0; i < 600; i++){
+   let state = VillageState.random();
+   totalTimeRobot1 += runRobotCompare(state, robot1,memory1);
+   totalTimeRobot2 += runRobotCompare(state, robot2,memory1);
+  }
+ console.log(`Robot 1 average turn is - ${totalTimeRobot1/600} `);
+ console.log(`Robot 2 average turn is - ${totalTimeRobot2/600}`);
+}
+
+compareRobots(yourRobot, [], goalOrientedRobot, []); // to compare two robots 
+
+// runRobot(VillageState.random(), yourRobot,[]); // use this to run one robot only  
+
+// you can also test it from console
+
